a39fc0ada3ed0aac59f46ccdc76e27c7d19df75c
Amnabibi123 <amnaawan5555@gmail.com>
2025-10-21 19:50:26 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,7 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// Import all necessary screen components
-import HomeScreen from './components/HomeScreen'; 
-import LuxuryPretScreen from './components/LuxuryPretScreen';
-import SummerCollectionScreen from './components/SummerCollectionScreen'; 
-// Note: You must ensure LuxuryPretScreen.js and SummerCollectionScreen.js exist in 'components'
-
-const Stack = createNativeStackNavigator();
+// App.js
+import React from "react";
+import AppNavigator from "./Navigation";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="Home" 
-        screenOptions={{ 
-          headerShown: false // Hides the header for all screens
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        {/* Winter Screen - MUST match 'LuxuryPret' route in HomeScreen.js */}
-        <Stack.Screen name="LuxuryPret" component={LuxuryPretScreen} /> 
-        {/* Summer Screen - MUST match 'SummerCollection' route in HomeScreen.js */}
-        <Stack.Screen name="SummerCollection" component={SummerCollectionScreen} /> 
-        {/* Add more screens here */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 }
